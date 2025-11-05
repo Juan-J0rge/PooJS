ad885b3695763edab017a8e4e9cf1ffdc420752a
Juan-J0rge <juancontatrabalho@gmail.com>
2025-11-05 20:38:28 -0300

    oopJs3

--- func.js
+++ func.js
@@ -10,10 +10,10 @@ function Pessoa(nome) {
   }
 }
 
-function Funcionario(nome, cargo, salario) {
+function Funcionario(nome, salario) {
   Pessoa.call(this, nome);
-  this.cargo = cargo;
-  
+  this.cargo = "Funcionário";
+
   let _salario = salario;
 
   this.chamarSalario = function() {
@@ -33,19 +33,57 @@ function Funcionario(nome, cargo, salario) {
   }
 }
 
-function Estagiario(nome) {
-  Funcionario.call(this, nome, "Estagiário", 2000);
+function Gerente(nome, salario) {
+  Pessoa.call(this, nome);
+  this.cargo = "Gerente";
+
+  let _salario = salario;
+
+  this.chamarSalario = function() {
+    return _salario;
+  }
+
+  this.setarSalario = function(valor) {
+    if (typeof valor === "number") {
+      _salario = valor;
+    } else {
+      console.log("Valor inválido!");
+    }
+  }
 
   this.aumento = function() {
-    const novoSalario = this.chamarSalario() * 1.07;
-    this.setarSalario(novoSalario);
+    _salario = _salario * 1.2;
+  }
+}
+
+function Estagiario(nome, salario) {
+  Pessoa.call(this, nome);
+  this.cargo = "Estagiário";
+
+  let _salario = salario;
+
+  this.chamarSalario = function() {
+    return _salario;
+  }
+
+  this.setarSalario = function(valor) {
+    if (typeof valor === "number") {
+      _salario = valor;
+    } else {
+      console.log("Valor inválido!");
+    }
+  }
+
+  this.aumento = function() {
+    _salario = _salario * 1.05;
   }
 }
 
 // Instâncias
 const pessoa1 = new Pessoa("Arlindo");
-const func1 = new Funcionario("Juan", "Desenvolvedor", 5000);
-const func2 = new Estagiario("JayJay");
+const func1 = new Funcionario("Juan", 5000);
+const gerente1 = new Gerente("JayJay", 8000);
+const estagiario1 = new Estagiario("Caio", 2000);
 
 // Testes
 pessoa1.dizOi();
@@ -57,8 +95,18 @@ console.log("Salário atual:", func1.chamarSalario());
 func1.aumento();
 console.log("Salário após aumento:", func1.chamarSalario());
 
-func2.dizOi();
-func2.dizCargo();
-console.log("Salário do estagiário:", func2.chamarSalario());
-func2.aumento();
-console.log("Salário do estagiário após aumento:", func2.chamarSalario());
+gerente1.dizOi();
+gerente1.dizCargo();
+console.log("Salário do gerente:", gerente1.chamarSalario());
+gerente1.aumento();
+console.log("Salário do gerente após aumento:", gerente1.chamarSalario());
+
+estagiario1.dizOi();
+estagiario1.dizCargo();
+console.log("Salário do estagiário:", estagiario1.chamarSalario());
+estagiario1.aumento();
+console.log("Salário do estagiário após aumento:", estagiario1.chamarSalario());
+
+// Observação:
+// O conteúdo de "class" e "extends" ainda não foi apresentado em aula.
+// Por isso, o exercício foi feito com funções construtoras e call() para representar a herança.

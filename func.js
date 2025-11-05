e25a54b5abf7c17097ea878b431504f3421d2088
Juan-J0rge <juancontatrabalho@gmail.com>
2025-11-05 18:57:21 -0300

    oopJs2

--- func.js
+++ func.js
@@ -1,57 +1,64 @@
 function Pessoa(nome) {
-    this.nome = nome;
-    this.dizOi = function() {
-        console.log("ola")
-    }
-    this.dizCargo = function() {
-        console.log(this.cargo);
-    }
+  this.nome = nome;
+
+  this.dizOi = function() {
+    console.log("Olá! Eu sou " + this.nome);
+  }
+
+  this.dizCargo = function() {
+    console.log(this.cargo ? this.cargo : "Cargo não definido.");
+  }
 }
 
-function funcionario(nome ,cargo, salario) {
-    this.cargo = cargo;
-    // this.salario = salario;
-    let _salario = salario;
+function Funcionario(nome, cargo, salario) {
+  Pessoa.call(this, nome);
+  this.cargo = cargo;
+  
+  let _salario = salario;
 
-    this.aumento = function() {
-        const novoSalario = _salario * 1.1;
-        _salario = novoSalario;
+  this.chamarSalario = function() {
+    return _salario;
+  }
+
+  this.setarSalario = function(valor) {
+    if (typeof valor === "number") {
+      _salario = valor;
+    } else {
+      console.log("Valor inválido!");
     }
+  }
 
-    this.chamarSalario = function() {
-        return _salario;
-    }
-
-    this.setarSalario = function(valor) {
-        if(typeof valor === 'number') {
-            _salario = valor;
-        }
-
-    }
-
-    Pessoa.call(this, nome);
+  this.aumento = function() {
+    _salario = _salario * 1.1;
+  }
 }
 
 function Estagiario(nome) {
-    funcionario.call(this, nome, "Estagiario", 2000);
+  Funcionario.call(this, nome, "Estagiário", 2000);
 
-    this.aumento = function() {
-        const novoSalario = this.chamarSalario() * 1.07;
-        this.setarSalario(novoSalario);
-    }
+  this.aumento = function() {
+    const novoSalario = this.chamarSalario() * 1.07;
+    this.setarSalario(novoSalario);
+  }
 }
 
+// Instâncias
+const pessoa1 = new Pessoa("Arlindo");
+const func1 = new Funcionario("Juan", "Desenvolvedor", 5000);
+const func2 = new Estagiario("JayJay");
 
-const Func1 = new funcionario("Juan", "Dev", 5000);
-const Pessoa1 = new Pessoa("Arlindo")
-const Func2 = new Estagiario("JayJay");
-Func1.setarSalario('mil');
-console.log(Func1);
-console.log(Pessoa1);
-Func1.dizOi();
-Func1.dizCargo();
-Func1.aumento();
-Func2.aumento();
+// Testes
+pessoa1.dizOi();
+pessoa1.dizCargo();
 
-console.log(Func2.chamarSalario());
-console.log(Func1.chamarSalario());
+func1.dizOi();
+func1.dizCargo();
+console.log("Salário atual:", func1.chamarSalario());
+func1.aumento();
+console.log("Salário após aumento:", func1.chamarSalario());
+
+func2.dizOi();
+func2.dizCargo();
+console.log("Salário do estagiário:", func2.chamarSalario());
+func2.aumento();
+console.log("Salário do estagiário após aumento:", func2.chamarSalario());
